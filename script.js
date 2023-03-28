e4be55a0405f6a449c17bec339616cb4bea4e8c2
Michael Partin <unmykx@gmail.com>
2023-03-28 11:52:58 -0700

    planning scroll button func

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const scrollLink = document.querySelector('#scroll-link')
+const bloxList = Array.from(document.querySelectorAll('.sixt-blox'))
+
 console.log('template scripts')
+
+scrollLink.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    let blockIndex = null
+
+    for( let block in bloxList ) {
+
+    }
+
+    console.log(bloxList)
+    console.log(e.target)
+})
