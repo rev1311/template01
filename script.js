6aa4176ebd4cce67164133de20ffd0e02e8b911f
MP <14315607+rev1311@users.noreply.github.com>
2023-03-31 17:45:27 -0700

    fixed scroll func for both buttons

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
-const scrollLink = document.querySelector('#scroll-link')
+const scrollLink1 = document.querySelector('#scroll-link1')
+const scrollLink2 = document.querySelector('#scroll-link2')
 
 scrollLink.addEventListener('click', (e) => {
     let linkId = e.target.href.substr(-1)
 
     switch(linkId) {
         case '0':
-            scrollLink.setAttribute('href', '#sixt-blox-2')
+            scrollLink1.setAttribute('href', '#sixt-blox-2')
+            scrollLink2.setAttribute('href', '#sixt-blox-2')
             break;
         case '#':
-            scrollLink.setAttribute('href', '#sixt-blox-2')
+            scrollLink1.setAttribute('href', '#sixt-blox-2')
+            scrollLink2.setAttribute('href', '#sixt-blox-2')
             break;
         case '2':
-            scrollLink.setAttribute('href', '#sixt-blox-3')
+            scrollLink1.setAttribute('href', '#sixt-blox-3')
+            scrollLink2.setAttribute('href', '#sixt-blox-3')
             break;
         case '3':
-            scrollLink.setAttribute('href', '#sixt-blox-4')
+            scrollLink1.setAttribute('href', '#sixt-blox-4')
+            scrollLink2.setAttribute('href', '#sixt-blox-4')
             break;
         case '4':
-            scrollLink.setAttribute('href', '#sixt-blox-5')
+            scrollLink1.setAttribute('href', '#sixt-blox-5')
+            scrollLink2.setAttribute('href', '#sixt-blox-5')
             break;
         case '5':
-            scrollLink.setAttribute('href', '#sixt-blox-6')
+            scrollLink1.setAttribute('href', '#sixt-blox-6')
+            scrollLink2.setAttribute('href', '#sixt-blox-6')
             break;
         case '6':
-            scrollLink.setAttribute('href', '#sixt-blox-7')
+            scrollLink1.setAttribute('href', '#sixt-blox-7')
+            scrollLink2.setAttribute('href', '#sixt-blox-7')
             break;
         case '7':
-            scrollLink.setAttribute('href', '#sixt-blox-8')
+            scrollLink1.setAttribute('href', '#sixt-blox-8')
+            scrollLink2.setAttribute('href', '#sixt-blox-8')
             break;
         case '8':
-            scrollLink.setAttribute('href', '#sixt-blox-0')
+            scrollLink1.setAttribute('href', '#sixt-blox-0')
+            scrollLink2.setAttribute('href', '#sixt-blox-0')
             break;
 
         default:
-            scrollLink.setAttribute('href', '#sixt-blox-0')
+            scrollLink1.setAttribute('href', '#sixt-blox-0')
+            scrollLink2.setAttribute('href', '#sixt-blox-0')
         }
 
 })
