a5ca730309fa3215133dac2d130ab34f5689890f
MP <14315607+rev1311@users.noreply.github.com>
2023-04-03 16:59:59 -0700

    refactored switch statement to ifs

--- script.js
+++ script.js
@@ -21,47 +21,15 @@ scrollLink2.addEventListener('click', (e) => {
 })
 
 function switchBoard(linkId) {
-    switch(linkId) {
-        case '0':
-            scrollLink1.setAttribute('href', '#sixt-blox-2')
-            scrollLink2.setAttribute('href', '#sixt-blox-2')
-            break;
-        case '#':
-            scrollLink1.setAttribute('href', '#sixt-blox-2')
-            scrollLink2.setAttribute('href', '#sixt-blox-2')
-            break;
-        case '2':
-            scrollLink1.setAttribute('href', '#sixt-blox-3')
-            scrollLink2.setAttribute('href', '#sixt-blox-3')
-            break;
-        case '3':
-            scrollLink1.setAttribute('href', '#sixt-blox-4')
-            scrollLink2.setAttribute('href', '#sixt-blox-4')
-            break;
-        case '4':
-            scrollLink1.setAttribute('href', '#sixt-blox-5')
-            scrollLink2.setAttribute('href', '#sixt-blox-5')
-            break;
-        case '5':
-            scrollLink1.setAttribute('href', '#sixt-blox-6')
-            scrollLink2.setAttribute('href', '#sixt-blox-6')
-            break;
-        case '6':
-            scrollLink1.setAttribute('href', '#sixt-blox-7')
-            scrollLink2.setAttribute('href', '#sixt-blox-7')
-            break;
-        case '7':
-            scrollLink1.setAttribute('href', '#sixt-blox-8')
-            scrollLink2.setAttribute('href', '#sixt-blox-8')
-            break;
-        case '8':
-            scrollLink1.setAttribute('href', '#sixt-blox-0')
-            scrollLink2.setAttribute('href', '#sixt-blox-0')
-            break;
-
-        default:
-            scrollLink1.setAttribute('href', '#sixt-blox-0')
-            scrollLink2.setAttribute('href', '#sixt-blox-0')
+    if(linkId == '0' || linkId == '#'){
+        scrollLink1.setAttribute('href', '#sixt-blox-2')
+        scrollLink2.setAttribute('href', '#sixt-blox-2')
+    } else if(linkId == '8'){
+        scrollLink1.setAttribute('href', '#sixt-blox-0')
+        scrollLink2.setAttribute('href', '#sixt-blox-0')
+    } else {
+        scrollLink1.setAttribute('href', `#sixt-blox-${linkId++}`)
+        scrollLink2.setAttribute('href', `#sixt-blox-${linkId++}`)
     }
 }
 
